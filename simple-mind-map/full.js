f20065f24affeed620731c960101766eb95d99be
ZhangMingZhao1 <1104272319@qq.com>
2024-10-15 14:46:21 +0800

    version: 0.0.6版本

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -29,7 +29,7 @@ MindMap.markdown = markdown
 MindMap.iconList = icons.nodeIconList
 MindMap.constants = constants
 MindMap.defaultTheme = defaultTheme
-MindMap.version = '0.0.5'
+MindMap.version = '0.0.6'
 
 MindMap.usePlugin(MiniMap)
   .usePlugin(Watermark)
