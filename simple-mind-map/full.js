697bec95105e8ef2cbd5b91a07325ca749b7d95a
ZhangMingZhao1 <1104272319@qq.com>
2024-11-07 17:44:09 +0800

    feat: version 0.0.11

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -29,7 +29,7 @@ MindMap.markdown = markdown
 MindMap.iconList = icons.nodeIconList
 MindMap.constants = constants
 MindMap.defaultTheme = defaultTheme
-MindMap.version = '0.0.10'
+MindMap.version = '0.0.11'
 
 MindMap.usePlugin(MiniMap)
   .usePlugin(Watermark)
